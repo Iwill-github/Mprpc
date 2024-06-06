a5c7f0b5fa42062055b4807c231a79cf227d4d60
wly <18538331073@163.com>
2024-06-06 15:53:00 +0800

    fix bug

--- autobuild.sh
+++ autobuild.sh
@@ -1,5 +1,14 @@
-cd ./build
-cmake ..
-make
+#!/bin/bash
+
+set -e          # 设置脚本在遇到任何命令失败时立即退出
+
+find `pwd`/build -type f ! -name "readme.txt" -delete
+find `pwd`/build -type d -empty -delete
+
+cd `pwd`/build &&
+    cmake .. &&
+    make
 cd ..
+
+cp -r `pwd`/src/include `pwd`/lib
 
